fb750fd0bdb4a6e54ab8d56fa54db389a3aec69c
Dheeraj Kumar <dheerajkumar7135227@gmail.com>
2024-08-15 22:27:57 +0530

    Update project.py
    Created Ranking module

--- project.py
+++ project.py
@@ -2,7 +2,7 @@ def display_welcome_message():
     print("+" + "-"*38 + "+")
     print("|" + " " * 11 + "Welcome to Cricbuzz" + " " * 11 + "|")
     print("|" + " " * 38 + "|")
-    print("|" + "Your one-stop destination for all things cricket!" + "|")
+    print("|" + "Your one-stop destination for all cricket related things...!" + "|")
     print("+" + "-"*38 + "+")
     print()
 
@@ -136,13 +136,47 @@ def main():
         elif choice == '6':
             while True:
                 display_rankings_menu()
-                sub_choice = input("Enter your choice (1-3): ")
+                sub_choice = input("\nEnter your choice: ")
                 if sub_choice == '1':
-                    print("Displaying Team Rankings...")
+                    print("\nDisplaying Team Rankings...")
+                    print("\n1. ODI Rankings")
+                    print("2. T20I Rankings")
+                    print("3. TEST Rankings")
+                    
+                    team_ranking_choice = input("\nEnter your choice: ")
+                    print("\n1. ODI Rankings")
+                    print("\n2. T20I Rankings")
+                    print("\n3. TEST Rankings")
+                    
+                    if team_ranking_choice == '1':   #1 = ODI, 2 = T20, 3 = TEST
+                        print("\nDisplaying Top 10 ODI Teams...\n1    India\n2    Australia\n3    South Africa\n4    Pakistan\n5    New Zealand\n6    Sri Lanka\n7    England\n8    Bangladesh\n9    Afghanistan\n10   West Indies")
+                    elif team_ranking_choice == '2':
+                        print("\nDisplaying Top 10 T20 Teams...\n1    India\n2    Australia\n3    England\n4    West Indies\n5    South Africa\n6    New Zealand\n7    Pakistan\n8    Sri Lanka\n9    Bangladesh\n10   Afghanistan")  
+                    elif team_ranking_choice == '3':
+                        print("\nDisplaying Top 10 TEST Teams...\n1    Australia\n2    India\n3    England\n4    South Africa\n5    New Zealand\n6    Pakistan\n7    Sri Lanka\n8    West Indies\n9    Bangladesh\n10   Ireland")
+                        
+                        
                 elif sub_choice == '2':
                     print("Displaying Player Rankings...")
-                elif sub_choice == '3':
-                    break
+                    print("1. ODI Rankings")
+                    print("2. T20I Rankings")
+                    print("3. TEST Rankings")
+                    
+                    player_ranking_choice = input("\nEnter your choice: ")
+                    print("1. ODI Rankings")
+                    print("2. T20I Rankings")
+                    print("3. TEST Rankings")
+
+                    if player_ranking_choice == '1':    #1 = ODI, 2 = T20, 3 = TEST
+                        print("\nDisplaying Top 10 ODI Batsmen...\n1    Babar Azam\n2    Rohit Sharma\n3    Shubhman Gill\n4    Harry Tector\n5    Virat Kohli\n6    Daryl Mitchell\n7    David Warner\n8    Pathum Nissanka\n9    Dawid Malan\n10   Rassie van der Dussen")
+                    elif player_ranking_choice == '2':    #1 = ODI, 2 = T20, 3 = TEST
+                        print("\nDisplaying Top 10 T20 Batsmen...\n1    Travis Head\n2    Surya Kumar Yadav\n3    Philip Salt\n4   Yashasvi Jaiswal\n5    Babar Azam\n6    Mohammad Rizwan \n7    Jos Butler\n8    Ruturaj Gaikwad\n9    Brandon King\n10   Johnson Charles")
+                    elif player_ranking_choice == '3':    #1 = ODI, 2 = T20, 3 = TEST
+                        print("\nDisplaying Top 10 TEST Batsmen...\n1    Joe Root\n2    Kane Williamson\n3    Babar Azam\n4    Daryl Mitchell\n5    Steven Smith\n6    Rohit Sharma\n7    Harry Brook\n8    Yashasvi Jaiswal\n9    Dimuth Karunaratne\n10   Virat Kohli")
+
+                        
+                    elif sub_choice == '3':
+                        break
                 else:
                     print("Invalid choice. Please enter a number between 1 and 3.")
 
@@ -154,3 +188,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
